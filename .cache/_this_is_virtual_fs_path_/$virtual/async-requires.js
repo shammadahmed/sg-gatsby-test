c03ce68f40659b58fae59364177788fb8b553e3a
Hammad Ahmed <shammad.c5@hotmail.com>
2022-10-15 21:48:06 +0500

    add node_env to env file

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,14 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=head" /* webpackChunkName: "component---src-pages-about-jshead" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */)
+}
+
